c71cc1fb49eb3a6427007e7e8edac19ce083b886
e-burkholder <e-burkholder@onu.edu>
2024-04-29 23:19:52 -0400

    fixed?

--- MP4/src/mp3/Trading.java
+++ MP4/src/mp3/Trading.java
@@ -1,14 +1,25 @@
 package mp3;
 
+import java.util.Random;
+
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+/**
+ * This class creates the trade function for the Oregon Trail game, along with calculating the various trading circumstances/values.
+ * 
+ * @author Elizabeth Burkholder
+ * @version 1.0
+ * @date 4/17/2024
+ * @filename Trading.java
+ */
 
 public class Trading extends Menu{
 	protected String name;
 	String names[] = {"trappers" , "natives" , "other travelers"};
-	String suppliesL = displaySupplies(foodAmt, numOxen);
+	String tradeOpts[] = {"oxen", "boxes of bullets", "sets of clothes", "pounds of food", "wagon wheels", "wagon tongues", "wagon axels"};
+	int tradeAmt1 = 0, tradeAmt2 = 0, randOpt1 = getRandomOpt(), randOpt2 = getRandomOpt(), currentWagNum;
 
-	
 	/*
 	 * Constructs an object of type Trading using an indexed number to determine 
 	 * what type of trader the player is interacting with.
@@ -17,51 +28,146 @@ public class Trading extends Menu{
 	public Trading(int idx) {	
 		name = this.names[idx];
 	}
-	
+
 	/*
 	 * Displays the trade text to the player through a JTextArea and calculates the multiplier based on distance from Oregon.
 	 * @param text - the main JTextArea used to communicate with the player.
 	 * @param trader - An object of type Trading, used to determine who the player is trading with.
 	 * @param multiplier - An integer value which determines how expensive the trade is (based on the wagon's distance).
-	 * @param onjPrice - an integer describing the prices of the given object.
-	 * @param obj - an object of type Item, used to determine which objects are being traded.
+	 * @param wagon - an object of type Wagon that contains inventory.
 	 */
-	public void initiateTrade(JTextArea text, Trading trader, int multiplier, int objPrice1, int objPrice2, Item obj1, Item obj2) {
-		text.setText(suppliesL + "\n\n The " + trader.name + " would like to trade\n " + (multiplier *objPrice1) + " " + obj1.getName() + " for " + (multiplier * objPrice2) 
-				+ " " + obj2.getName() + ".\n\n Would you like to trade? (yes or no)\n");
+	public void initiateTrade(JTextArea text, Trading trader, int multiplier, Wagon wagon) {
+		randOpt1 = getRandomOpt();
+		randOpt2 = getRandomOpt();
+		tradeAmt1 = getTradeAmount(tradeAmt1, randOpt1);
+		tradeAmt2 = getTradeAmount(tradeAmt2, randOpt2);
+		text.setText(" Oxen: " + wagon.numOxen + "\n Boxes of Bullets: " + (wagon.ammunitionAmt/20) + "\n Sets of Clothing: " + wagon.clothSetNum + "\n Food: " + wagon.foodAmt 
+				+ "\n Wagon Wheels: " + wagon.wheelNum + "\n Wagon Tongues: " + wagon.tongueNum + "\n Wagon Axels: " + wagon.axelNum + "\n\n The " + trader.name + " would like to trade\n " + (multiplier *tradeAmt1) + " " + tradeOpts[randOpt1] + " for " + (multiplier * tradeAmt2) 
+				+ " " + tradeOpts[randOpt2] + ".\n\n Would you like to trade? (yes or no)\n");
 	}
-	
+
 	/*
 	 * A Boolean function to determine whether the player input == "yes", "no", or something else. Updates wagon if "yes".
 	 * @param input - A string representing the player's answer to prompt.
 	 * @param in - A JTextField that ___.
 	 * @param text - The main JTextArea for the game's communication with player.
+	 * @param wagon - An object of type Wagon used to determine the player's inventory.
 	 * @return - Returns false when the player answers "yes" or "no". Otherwise, returns true.*/
-	public Boolean tradeMenu(JTextArea text, JTextField in, String input) {
-		if (input.equals("yes")) {
-			//supplies.addItem(obj); 		//adding the item to the wagon
+	public Boolean tradeMenu(JTextArea text, JTextField in, String input, Wagon wagon, int randOpt2, int tradeAmt2) {
+		// Debugging Purposes - System.out.println(" trading option: " + randOpt2);
+		currentWagNum = getCurrentWNum(randOpt2, wagon);
+		
+		//Debugging Purposes - System.out.println(" number of thing in wagon: " + currentWagNum);
+		if(currentWagNum < tradeAmt2) {
+			System.out.println("You cannot trade, not enough items.");
+			return false;
+		}
+		else if (input.equals("yes") && currentWagNum >= tradeAmt2) {
 			System.out.println("item added.");
-			text.setText(" Trade complete! Press enter to continue");
 			return false;
 		}
 		else if (input.equals("no")){
-			baseMenu(text, true);
+			text.setText(" You said no.");
 			return false;
 		}
-		else {return true;}
+		else return true;
+	}
+
+	/*
+	 * Gets a random object name and a random value using the object name using a switch statement.
+	 * @param tradePrice - a temporary integer value to store the final price of the traded value.
+	 * @param randOpt - an integer value which determines which object is being traded to help determine trade prices.
+	 * @return tradePrice - an integer value describing the amount of the corresponding object will be traded.
+	 */
+	public int getTradeAmount(int tradePrice, int randOpt){
+		//Calculates the range for an object's price without accounting for multipliers.
+		switch (randOpt) {
+		case 0: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+			
+		case 1: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		case 2: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		case 3: 
+			tradePrice = (int)(Math.random()*((120-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		case 4: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		case 5: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		case 6: 
+			tradePrice = (int)(Math.random()*((5-1)+1))+1;
+			System.out.println(tradeOpts[randOpt] + " " + tradePrice);
+			return tradePrice;
+
+		default:
+			System.out.println("plplplplplplplplplplpl");
+			tradePrice = 0;
+			return tradePrice;
+		}
+
+}
+
+	/*
+	 * Gets a random number to use for the tradeOption array.
+	 * @return num - an integer value between 0 and 7
+	 */
+	public int getRandomOpt() {
+		Random randomOpt = new Random();
+		int num = randomOpt.nextInt(7);
+		return num;
 	}
 	
 	/*
-	 *Displays the supplies to the user
-	 *@param food - an integer value representing the amount of food the player currently has (in lbs).
-	 *@param oxen - an integer value representing the number of oxen present in the player's party.
-	 *@param (eventually) - int setsClothes, int wagTongues, int wagWheels, int bullets
+	 * Gets the current amount of the object in the user's wagon using an identifier integer.
+	 * @param randOption - an integer representing the item to determine how much is within the wagon. 
+	 * @param wagon - An object of type Wagon, containing the methods to obtain the inventory.
+	 * @return wagon.___ - an integer value that varies depending on which object was counted.
 	 */
-	//hopefully/preferably everything except food, text, and oxen are part of a Supplies Array or smt.
-	public String displaySupplies(int food, int oxen) {
-		String supplies = " Oxen: " + oxen + "\n Boxes of Bullets: " + "\n Sets of Clothing: " + "\n Food: " + food 
-				+ "\n Wagon Wheels: " + "\n Wagon Tongues: " + "\n Wagon Axels: ";
-		return supplies;
+	public int getCurrentWNum(int randOption, Wagon wagon) {
+		switch(randOption) {
+		case 0: return wagon.numOxen;
+		case 1: return wagon.ammunitionAmt/20;
+		case 2: return wagon.clothSetNum;
+		case 3: return wagon.foodAmt;
+		case 4: return wagon.wheelNum;
+		case 5: return wagon.tongueNum;
+		case 6: return wagon.axelNum;
+		default: return 314159265;
+		}
+	}
+
+	/*
+	 * A value containing the random object the user is offering up.
+	 * @return randOpt2 - an integer value containing the current value of the randOpt2.
+	 */
+	public int getrandOptNum2() {
+		return randOpt2;
 	}
 	
+	/*
+	 * A value containing the amount of another item that the other party has requested.
+	 * @return tradeAmt2 - an integer value determined by previous code within the Trading class.
+	 */
+	public int gettradeAmt2() {
+		return tradeAmt2;
+	}
+
 }
--- MP4/src/mp3/Wagon.java
+++ MP4/src/mp3/Wagon.java
@@ -7,16 +7,15 @@ import javax.swing.JTextArea;
 
 /**
  * The wagon for an implement of the game Oregon Trail.
- * A subclass of Item
- * 
+ * A subclass of Item.
  * @author Griffin Broge
  * @version 1.0.0
  * @date March 24th 2024
  * @filename Wagon.java
  */
 
-public class Wagon extends Item{
-	// Initialize Variables
+public class Wagon{
+	// Initialize Variables.
 	protected int wagonMax = 2400;
 	protected int wagonWeight = 0;
 	protected int foodAmt = 0;
@@ -26,95 +25,184 @@ public class Wagon extends Item{
 	private double location = 0;
 	private double nextLoc = 0;
 	private double numPace = 1;
-	protected int numOxen = 4;
-	private int money = 1000;
+	protected int numOxen = 0;
+	private double money = 1000;
 	private int consumption;
-    // Items are stored in an array list
+	protected int clothSetNum = 0;
+	protected int ammunitionAmt = 0;
+	private double totalBill = 0;
+	protected int wheelNum = 0;
+	protected int axelNum = 0;
+	protected int tongueNum = 0;
+
+	// Items are stored in an array list.
 	ArrayList<Item> supplies = new ArrayList<Item>();
+	
+
 	/**
 	 * Default Constructor
 	 */
-	public Wagon() {
+	public Wagon() {}
 	
+	public void setWheelNum(int wheelNum) {
+		this.wheelNum = wheelNum;
 	}
-	/**
-	 * Adds an item to the array list
-	 * @param item The item you want to add
+	
+	public int getWheelNum() {
+		return wheelNum;
+	}
+	
+	public void setAxelNum (int axelNum) {
+		this.axelNum = axelNum;
+	}
+	
+	public int getAxelNum() {
+		return axelNum;
+	}
+	
+	public void setTongueNum(int tongueNum) {
+		this.tongueNum = tongueNum;
+	}
+	
+	public int getTongueNum() {
+		return tongueNum;
+	}
+
+	/*
+	 * Gets the bill value.
+	 * @return totalBill - returns a double.
+	 */
+	
+	public  double getTotalBill() {
+		return totalBill;
+	}
+
+	/*
+	 * Sets the ammunition based on bullets/boxes.
+	 * @param ammo - an integer value for ammo. 
+	 */
+	public void setAmmo(int ammo) {
+		ammunitionAmt = ammunitionAmt + (20*ammo);
+	}
+
+	/*
+	 * Gets the ammo amount.
+	 * @return ammunitionAmt - an integer for the amount of ammunition.
+	 */
+	public int getAmmo() {
+		return ammunitionAmt;
+	}
+
+	/*
+	 * Sets the food amount value.
+	 * @param food - an integer value containing the amount of food in wagon.
+	 */
+	public void setFoodAmt(int food) {
+		foodAmt = foodAmt + food;
+	}
+
+	/*
+	 * Adds item to wagon.
+	 * @param item - an object of type Item used to determine what to pass to wagon.
 	 */
 	public void addItem(Item item) {
-		
 		supplies.add(item);
 	}
+
 	/**
-	 * Removes an item from the array list
-	 * @param item The item you want to remove
+	 * Removes an item from the array list.
+	 * @param item The item you want to remove.
 	 */
 	public void removeItem(Item item) {
-		
 		supplies.remove(item);
 	}
+
 	/**
-	 * Used to update the total weight of the wagon
+	 * Used to update the total weight of the wagon.
 	 */
 	public void updateWeight() {
-		
-			wagonWeight = 0;
+
+		wagonWeight = 0;
 		supplies.forEach((n -> {
 			wagonWeight += n.getWeight();
 		}));
 	}
+
 	/**
-	 * Getter for the current weight of the class
+	 * Getter for the current weight of the class.
 	 */
 	public int getWeight() {
-		
 		return wagonWeight;
 	}
+
 	/**
-	 * Getter for the maximum weight for the wagon
+	 * Getter for the maximum weight for the wagon.
 	 * @return the max weight for the wagon
 	 */
 	public int getWagonMax() {
-		
 		return wagonMax;
 	}
+
 	/**
-	 * Calculates the amount of food in the wagon in lbs
-	 * @return the amount of food in the wagon
+	 * Calculates the amount of food in the wagon in lbs.
+	 * @return the amount of food in the wagon.
 	 */
 	public int getFoodAmt() {
-
-		
 		foodAmt = 0;
 		supplies.forEach((n -> {
 			if(n.getEdibility() == true) {
 				foodAmt += n.getWeight();
 			}
 		}));
-		
 		return foodAmt;
 	}
-	
+
+	/*
+	 * Gets the food number as an integer.
+	 * @return foodAmt - an integer representing the food in the wagon.
+	 */
 	public int getFoodNum() {
 		return(foodAmt);
 	}
-	
+
+	/*
+	 * Clears the wagon of all supplies.
+	 */
 	public void clearWagon() {
 		supplies.clear();
 	}
+
+	/*
+	 * Gets the weather at the point when called.
+	 * @return weather - a string representing the current weather.
+	 */
 	public String getWeather() {
 		return(weather);
 	}
+
+	/*
+	 * Gets the pace of the wagon.
+	 * @return pace - the pace of the wagon as a string.
+	 */
 	public String getPace() {
 		return(pace);
 	}
+
+	/*
+	 * Gets the pace as a double value.
+	 * @return numPace - an double value representing the speed of the wagon.
+	 */
 	public double getNumPace() {
 		return(numPace);
 	}
+
+	/*
+	 * Calculates the pace of the wagon with values and strings.
+	 */
 	public void calcPace() {
 		double temp = 1;
 		double temp2 = 1;
-		
+
 		if(pace.equals("Steady")) {
 			temp = 1;
 		}
@@ -124,18 +212,19 @@ public class Wagon extends Item{
 		else if (pace.equals("Grueling")) {
 			temp = 2;
 		}
-		
+
 		if(numOxen < 4) {
 			temp2 = numOxen / 4;
 		}
-		
 		numPace = 20 * temp * temp2;
-		
 	}
-	
+
+	/*
+	 * Calculates the food usage of certain strings to integers.
+	 */
 	public void calcFood() {
 		int temp = 1;
-		
+
 		if(rations.contains("Bare")) {
 			temp = 1;
 		}
@@ -145,22 +234,50 @@ public class Wagon extends Item{
 		if(rations.contains("Fill")) {
 			temp = 3;
 		}
-		
+
 		consumption = 5 * temp;
-		
+
 	}
+
+	/*
+	 * Sets the pace the wagon.
+	 * @param newPace - a string containing the pace.
+	 */
 	public void setPace(String newPace) {
 		pace = newPace;
 	}
+
+	/*
+	 * Gets the ration amount from the wagon.
+	 * @return rations - a string containing the rations.
+	 */
 	public String getRations() {
 		return(rations);
 	}
+
+	/*
+	 * Sets the rations using the new rations string value.
+	 * @param newRations - a string containing the description of the ration value. 
+	 */
 	public void setRations(String newRations) {
 		rations = newRations;
 	}
+
+	/*
+	 * Gets the location on a certain point on the trail.
+	 * @return location - a double value containing the user's location.
+	 */
 	public double getLocation() {
 		return(location);
 	}
+
+	/*
+	 * Decrements the food while traveling and updates the user's location.
+	 * @param evt - an actionEvent used to determine when to execute.
+	 * @param text - a JTextArea for the user input.
+	 * @param food - an integer value containing the amount of food in the wagon.
+	 * @param nextLandmark - an integer value to determine how far until the next landmark appears.
+	 */
 	public void travel(ActionEvent evt, JTextArea text, int food, int nextLandmark)
 	{	
 		calcFood();
@@ -171,22 +288,86 @@ public class Wagon extends Item{
 		+ "\n Food: " + getFoodNum() + "\n Press q to stop"
 		+ "\n Distance to next location: " + nextLoc);
 	}
-	
+
+	/*
+	 * Sets the number of oxen to the inputted value.
+	 * @param Oxen - an integer value containing the desired number of oxen for the party. 
+	 */
+	public void setOxen(int Oxen) {
+		numOxen = numOxen + (2*Oxen);
+	}
+
+	/*
+	 * Sets the number of clothes to the inputted value.
+	 * @param setNum - an integer value containing the desired number of sets of clothes for the party.
+	 */
+	public void setClothSet(int setnum) {
+		clothSetNum = clothSetNum + setnum;
+	}
+
+	/*
+	 * Gets the number of clothes to the inputted value.
+	 * @return clothSetNum - an integer value containing the number of sets of clothes in the wagon. 
+	 */
+	public int getClothSet() {
+		return clothSetNum;
+	}
+
+	/*
+	 * Gets the number of oxen to the inputted value.
+	 * @return numOxen - an integer value containing the number of oxen in the party. 
+	 */
 	public int getOxen() {
 		return(numOxen);
 	}
-	
-	public int getMoney() {
+
+	/*
+	 * Calculates the money in the party.
+	 * @param price - a double value for the price of an object.
+	 * @param input - an integer value for the number of items desired.
+	 */
+	public void calcMoney(double price, int input) {
+		money = money -(price * input);
+	}
+
+	/*
+	 * Calculates the total bill for the shop.
+	 */
+	public void setTotalBill() {
+		totalBill = 0;
+	}
+
+	/*
+	 * Calculates the total bill using the price and inputted number.
+	 * @param price - a double value representing the price of the total bill
+	 * @input - an input presented by the user.
+	 */
+	public void calcTotalBill(double price, int input) {
+		totalBill = totalBill + (price * input);
+	}
+
+	/*
+	 * Gets the amount of money left over
+	 * @return money - a double value containing the current user's total money.
+	 */
+	public double getMoney() {
 		return(money);
 	}
-	
+
+	/*
+	 * Decrements the food amount by the consumption.
+	 */
 	public void eatFood() {
 		foodAmt -= consumption;
 	}
-	
+
+	/*
+	 * Gets the locations of the next landmark.
+	 * @param nextLandmark - an integer value determining the next landmark.
+	 * @return nextLoc - a double value representing the next locations coordinate distance.
+	 */
 	public double getNextLocation(int nextLandmark) {
-		nextLoc = nextLandmark - location;
+		nextLoc = nextLandmark - getLocation();
 		return(nextLoc);
 	}
-	
 }
